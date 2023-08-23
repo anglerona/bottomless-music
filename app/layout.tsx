87161a167dc7975dce9139cf6cc58687f336ff42
Angelina <angelina03wu@gmail.com>
2023-08-23 02:00:09 -0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+
+const font = Roboto({ weight: '400', subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'BottomlessMusic',
+  description: 'Enjoy all music without any limits',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
